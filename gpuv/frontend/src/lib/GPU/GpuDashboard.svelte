e9a2249670ff71272e267caa6f7a6ebcee7d0a25
Jared Wilber <jdwlbr@gmail.com>
2023-08-22 21:27:34 -0700

    integrate nvml

--- gpuv/frontend/src/lib/GPU/GpuDashboard.svelte
+++ gpuv/frontend/src/lib/GPU/GpuDashboard.svelte
@@ -2,36 +2,39 @@
   import { writable } from "svelte/store";
   import { onMount } from "svelte";
   import SharedLine from "./SharedLine.svelte";
-  import { gpuData } from "./data.js";
+  // import { gpuData } from "./data.js";
 
-  // let initialDeviceData = gpu_data["device_status"];
-  // let initialData = initialDeviceData.map((d) => ({
-  //   timestamp: d.timestamp,
-  //   gpu: d.utilization.gpu,
-  //   memory: d.utilization.memory,
-  // }));
+  export const gpuSvelteStore = writable([]);
 
-  // Define the writable store
-
-  async function retrieveDBData() {
-    const response = await fetch("/chat/comparator/db/retrieve");
-    const data = await response.json();
-    console.log("uploooo", data);
-
-    // const data = await response.json();
-    //     console.log("uploooo", data);
-
-    //     // Assuming that the returned data has a similar structure to gpu_data.device_status
-    //     const formattedData = data.device_status.map((d) => ({
-    //       timestamp: d.timestamp,
-    //       gpu: d.utilization.gpu,
-    //       memory: d.utilization.memory,
-    //     }));
-
-    //     dataStore.set(formattedData);
+  async function fetchGpuData() {
+    try {
+      const response = await fetch("/nvml");
+      const data = await response.json();
+      return data.device_status.map((device) => ({
+        timestamp: device.timestamp,
+        gpu: device.utilization.gpu,
+        memory: device.utilization.memory,
+        temperature: device.temp_fan.temperature,
+        fan_speed: device.temp_fan.fan_speed,
+        current_graphics_clock: device.clocks_pstate.current_graphics_clock,
+        current_memory_clock: device.clocks_pstate.current_memory_clock,
+      }));
+    } catch (error) {
+      console.error("Failed to fetch GPU data", error);
+      return [];
+    }
   }
 
-  let interval;
+  onMount(() => {
+    // Fetch data every 1000ms
+    console.log("mounting!");
+    const interval = setInterval(async () => {
+      const newData = await fetchGpuData();
+      gpuSvelteStore.update((data) => [...data, ...newData]);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
 
   let vals = [
     { y: "gpu", title: "GPU", yAxis: "GPU" },
@@ -50,36 +53,7 @@
     },
   ];
 
-  onMount(() => {
-    interval = setInterval(() => {
-      let newTimestamp = new Date().toISOString();
-      let newGpuValue = Math.floor(Math.random() * 100);
-      let newMemoryValue = Math.floor(Math.random() * 100);
-      let newTemperature = Math.floor(Math.random() * 100);
-      let newFanSpeed = Math.floor(Math.random() * 100);
-      let newGraphicsClock = Math.floor(Math.random() * 100);
-      let newMemoryClock = Math.floor(Math.random() * 100);
-
-      // Update the store with the new data
-      gpuData.update((data) => [
-        ...data,
-        {
-          timestamp: newTimestamp,
-          gpu: newGpuValue,
-          memory: newMemoryValue,
-          temperature: newTemperature,
-          fan_speed: newFanSpeed,
-          current_graphics_clock: newGraphicsClock,
-          current_memory_clock: newMemoryClock,
-        },
-      ]);
-    }, 500);
-
-    // Cleanup the interval when component is destroyed
-    return () => {
-      clearInterval(interval);
-    };
-  });
+  $: console.log($gpuSvelteStore);
 </script>
 
 <h3>GPU Monitoring</h3>
@@ -87,7 +61,7 @@
 <div id="chart">
   {#each vals as d}
     <SharedLine
-      data={$gpuData}
+      data={$gpuSvelteStore}
       x="timestamp"
       y={d.y}
       yAxisLine="false"
--- gpuv/server.py
+++ gpuv/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 from flask_cors import CORS
+import gpuv
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +18,11 @@ def home(path):
     return send_from_directory("frontend/dist", path)
 
 
+@app.route("/nvml")
+def nvml_endpoint():
+    nv = gpuv.Nvml()
+    return jsonify(nv.get())
+
+
 if __name__ == "__main__":
-    print("hey!")
     app.run(debug=False)
